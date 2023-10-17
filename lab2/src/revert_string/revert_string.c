a7cf81fdd75225944a44315c14bd152a74dbb897
polina <null>
2023-10-17 22:44:43 +0000

    revert_string.c, added rev_str

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,16 @@
+#include <string.h>
 #include "revert_string.h"
 
 void RevertString(char *str)
 {
-	// your code here
+    char temp1, temp2;
+    int length = strlen(str);
+    for (int i=length/2; i>0; i--)
+    {
+        temp1 = str[i-1];
+        temp2 = str[length-i];
+        str[i-1] = temp2;
+        str[length-i] = temp1;
+   }
 }
 
