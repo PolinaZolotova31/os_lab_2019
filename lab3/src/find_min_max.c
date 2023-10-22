bac4e5c8fb6bd5ec8616c0c5a8f36deba291670e
polina <null>
2023-10-22 19:20:02 +0000

    Added find_min_max.c makefile parallel_min_max.c run_exec run_exec.c sequential_min_max utils.o

--- lab3/src/find_min_max.c
+++ lab3/src/find_min_max.c
@@ -7,6 +7,17 @@ struct MinMax GetMinMax(int *array, unsigned int begin, unsigned int end) {
   min_max.min = INT_MAX;
   min_max.max = INT_MIN;
 
-  // your code here
+    while(begin <= end)
+    {
+        if(array[begin] > min_max.max)
+        {
+            min_max.max = array[begin];
+        }
+        if(array[begin] < min_max.min){
+            min_max.min = array[begin];
+        }
+        ++begin;
+    }
+    
   return min_max;
 }
